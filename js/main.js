081b98f78626cdfc8f72853d4b62ab76aceb2f22
mu-777 <ryosuke.murata.7@gmail.com>
2018-03-21 17:59:23 +0900

    Add age average line

--- js/main.js
+++ js/main.js
@@ -44,6 +44,7 @@ var agesLineChart = function (names, ages) {
             labels: names,
             datasets: [
                 {
+                    label: 'Age',
                     data: ages,
                     backgroundColor: "rgba(255, 99, 132, 0.5)",
                     borderColor: "rgba(255, 99, 132, 1.0)",
@@ -54,8 +55,49 @@ var agesLineChart = function (names, ages) {
         options: {
             responsive: true,
             title: {display: false, text: 'Ages'},
-            scales: {yAxes: [{ticks: {min: 0}}]}
-        }
+            legend: {display: false},
+            scaleLabel: {display: false},
+            scales: {yAxes: [{ticks: {beginAtZero: true}}]}
+        },
+        plugins: [{
+            afterDatasetsUpdate: function (chart, options) {
+                var averages = chart.config.data.datasets.map(function (dataset, idx, arr) {
+                    var filtered = dataset.data.filter(function (val) {
+                            return !isNaN(val)
+                        }),
+                        sum = filtered.reduce(function (prev, curr) {
+                            return prev + curr
+                        });
+                    return sum / filtered.length;
+                });
+                chart.config.options.averages = averages;
+            },
+            afterRender: function (chart, options) {
+                if (!chart.config.options.averages) {
+                    return;
+                }
+                var ctxPlugin = chart.chart.ctx,
+                    datasets = chart.config.data.datasets,
+                    xAxe = chart.scales[chart.config.options.scales.xAxes[0].id],
+                    yAxe = chart.scales[chart.config.options.scales.yAxes[0].id];
+
+                chart.config.options.averages.forEach(function (average, idx, arr) {
+                    var averagePx = yAxe.getPixelForValue(average);
+                    ctxPlugin.strokeStyle = datasets[idx].borderColor;
+                    ctxPlugin.beginPath();
+                    ctxPlugin.moveTo(xAxe.left, averagePx);
+                    ctxPlugin.lineTo(xAxe.right, averagePx);
+                    ctxPlugin.stroke();
+
+                    ctxPlugin.fillStyle = datasets[idx].borderColor;
+                    ctxPlugin.font = Chart.helpers.fontString(20, 'normal', 'default');
+                    ctxPlugin.textAlign = 'right';
+                    ctxPlugin.textBaseline = 'bottom';
+                    ctxPlugin.fillText('Average: ' + average.toFixed(2),
+                        xAxe.right - 2, averagePx - 5);
+                });
+            }
+        }]
     });
 };
 
